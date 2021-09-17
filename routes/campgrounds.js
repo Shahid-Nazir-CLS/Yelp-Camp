27522d323b3565f96acf4d6e935b8f701666ddcd
shahid <shahid@yahoo.com>
2021-09-18 00:13:16 +0530

    added review stars functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,24 +7,19 @@ const campgrounds = require("../controllers/campgrounds")
 
 
 // INDEX ROUTE -- SHOW ALL CAMPGROUNDS
-router.get("/", catchAsync(campgrounds.index))
+router.route("/")
+    .get( catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-// NEW ROUTE -- SHOW FORM TO CREATE NEW CAMPGROUNDS
+
 router.get("/new", isLoggedIn, campgrounds.renderForm)
 
-// CREATE ROUTE -- ADD NEW CAMPGROUND
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.route("/:id")
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground));
 
-// SHOW ROUTE -- show info about a campground
-router.get("/:id", catchAsync(campgrounds.showCampground));
-
-// Edit Route
 router.get("/:id/edit", isLoggedIn,isAuthor ,catchAsync(campgrounds.renderEditForm));
 
-// Update Route
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground));
-
-// Destroy Route
 router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,13 @@ const catchAsync = require("../utils/catchAsync");
 const users = require("../controllers/users");
 
 
+router.route("/register")
+    .get(users.renderRegisterForm)
+    .post(catchAsync(users.createUser)); 
 
-router.get("/register", users.renderRegisterForm);
-
-router.post("/register", catchAsync(users.createUser)); 
-
-router.get("/login", users.renderLoginForm);
-
-router.post("/login", passport.authenticate('local', {failureFlash: true, 
+router.route("/login")
+    .get(users.renderLoginForm)
+    .post(passport.authenticate('local', {failureFlash: true, 
     failureRedirect: '/login'}), catchAsync(users.loginUser)); 
 
 router.get("/logout", users.logoutUser)
